85c41a7546bb31b37ee37fb782a0a998010f50e3
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-19 23:06:41 +0000

    adding shadcn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,35 @@
+'use client'
 import Image from "next/image";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import Button from "./Button";
+
 
 export default function Header() {
+
   return (
     <div className="flex items-center justify-between p-4 text-primary">
       <Image src="/logo.svg" alt="WApprentice" width={100} height={100} />
-      <nav className="flex flex-row">
-        <ul className="flex flex-row">
-          <li>Home</li>
-          <li>Find Apprenticeships</li>
-          <li>Ratings & Reviews</li>
-          <li>Salary Insights</li>
-          <li>Events & Networking</li>
-          <li>Resources</li>
-        </ul>
-      </nav>
+      <NavigationMenu className="flex flex-row">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <Button type={'primary'} text='Join the Waitlist'/>
     </div>
   );
 }
