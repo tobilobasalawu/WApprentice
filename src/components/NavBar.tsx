1e809d4e6f8624a22b4e063fc513c2871c80bd7f
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-19 19:05:59 +0000

    creating Button Component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,14 @@
 export default function NavBar() {
     return (
         <nav>
+          <img src="src/assets/logo.svg"/>
             <ul>
                 <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
+                <li>Find Apprenticeships</li>
+                <li>Ratings & Reviews</li>
+                <li>Salary Insights</li>
+                <li>Events & Networking</li>
+                <li>Resources</li>
             </ul>
         </nav>
     );
