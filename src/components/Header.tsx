529bc6311d4804752d689de3e34428059d3a1f89
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-19 22:37:23 +0000

    creating Button Component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,17 @@ import Image from "next/image";
 export default function Header() {
   return (
     <div className="flex items-center justify-between p-4 text-primary">
-      <Image src={"/logo.svg"} alt="WApprentice" width={100} height={100} />
+      <Image src="/logo.svg" alt="WApprentice" width={100} height={100} />
+      <nav className="flex flex-row">
+        <ul className="flex flex-row">
+          <li>Home</li>
+          <li>Find Apprenticeships</li>
+          <li>Ratings & Reviews</li>
+          <li>Salary Insights</li>
+          <li>Events & Networking</li>
+          <li>Resources</li>
+        </ul>
+      </nav>
     </div>
   );
 }
