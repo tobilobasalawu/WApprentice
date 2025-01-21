221f50ed8d7328641c84d3f5eb45f1875bf402f0
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-21 23:40:50 +0000

    creating a hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from '@/components/Header';
 import React from 'react';
+import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
