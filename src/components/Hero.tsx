fc70bd97f7799f7372b93a7caa98b74eb9a20535
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-22 19:16:38 +0000

    figured out the responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 export default function Hero() {
   return (
-  <div className="max-w-5xl mx-auto text-center">
+  <div className="max-w-5xl mx-auto text-center mt-10">
       <div className="flex flex-wrap justify-center">
         <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold tracking-tight">
           <span className="block sm:inline mr-2 text-[var(--special)]">Discover, Apply, Track </span>
@@ -12,14 +12,16 @@ export default function Hero() {
         </h2>
       </div>
 
-      <p className="paragraph-hero text-center mt-1 text-sm px-2">
+      <p className="paragraph-hero text-center text-sm px-2">
         Find, apply, and track apprenticeships in one click—no BS. Search by location, levels, and sector, and get the real tea on employers, salary comparisons, and CV tips. Check apprenticeship salary levels and see how they grow year by year. Stay on top with progress updates, notifications, and balance applications with your A-Levels like a boss. Enough with the struggles—forget LinkedIn oppression, rejections are nothing, and slay your career with exclusive events, insider insights, and top-secret resources.
       </p>
 
-      <div className='flex flex-col sm:flex-row items-center justify-center space-y-4 sm:space-y-0 sm:space-x-4 mt-5 w-full'>
-        <Input placeholder="Enter Email" className="w-[40vw]" />
-        <Button type={'secondary'} text={'Join Waitlist'} />
+      <div className='flex flex-col sm:flex-row items-center justify-center space-y-4 sm:space-y-0 sm:space-x-4 mt-5 mb-5 w-full'>
+        <Input placeholder="First Name" className="w-full px-4 py-3" />
+        <Input placeholder="Email Address" className="w-full px-4 py-3" />
       </div>
+
+      <Button type={'secondary'} text={'Join Waitlist'}/>
 
     </div>
   );
