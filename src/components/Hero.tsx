f7f0e89dc684d3c3a82b306b91ed148ca5980807
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-22 14:44:05 +0000

    styling hero components

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,12 @@
+import Button from './Button';
+
 export default function Hero() {
-  return(
-    <div>
-      <h1>Hero</h1>
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="hero-title">Discover, Apply, and Track Your Dream Apprenticeship with Ease</h1>
+      <p className="">
+        Find, apply, and track apprenticeships in one click—no BS. Search by location, levels, and sector, and get the real tea on employers, salary comparisons, and CV tips. Check apprenticeship salary levels and see how they grow year by year. Stay on top with progress updates, notifications, and balance applications with your A-Levels like a boss. Enough with the struggles—forget LinkedIn oppression, rejections are nothing, and slay your career with exclusive events, insider insights, and top-secret resources.
+      </p>
     </div>
-  )
+  );
 }
