3ebf4d605f039198a2d0d18631979cc739fc4285
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-23 19:58:00 +0000

    adding resposnive for hero2 image

--- src/components/HeroSecond.tsx
+++ src/components/HeroSecond.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function HeroSecond() {
   return (
-    <div>
-      <Image src={'search.svg'} width={'526'} height={'296'}/>
-      <Image src={'rangedisplay.svg'} width={'526'} height={'296'}/>
+    <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center' }}>
+      <Image src={'search.svg'} width={'526'} height={'296'} style={{ margin: '10px' }}/>
+      <Image src={'rangedisplay.svg'} width={'526'} height={'296'} style={{ margin: '10px' }}/>
     </div>
     
   );
