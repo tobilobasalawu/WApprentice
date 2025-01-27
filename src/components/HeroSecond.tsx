fe7882e1c052a1230c6fd3f4022139fb18fef303
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-27 21:10:48 +0000

    added a padding in front and end of images in hero section

--- src/components/HeroSecond.tsx
+++ src/components/HeroSecond.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 export default function HeroSecond() {
   return (
     <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center' }}>
-      <Image src={'search.svg'} width={'526'} height={'296'} style={{ margin: '10px' }}/>
-      <Image src={'rangedisplay.svg'} width={'526'} height={'296'} style={{ margin: '10px' }}/>
+      <div style={{ margin: '10px', marginLeft: '5%', marginRight: '5%' }}>
+        <Image src={'search.svg'} width={'526'} height={'296'} />
+      </div>
+      <div style={{ margin: '10px', marginLeft: '5%', marginRight: '5%' }}>
+        <Image src={'rangedisplay.svg'} width={'526'} height={'296'} />
+      </div>
     </div>
-    
   );
 }
