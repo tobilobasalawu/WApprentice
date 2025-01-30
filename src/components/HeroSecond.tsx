018260b40d27a34a363cbcc804c60c29d4caec6e
Tobi <155114173+tobilobasalawu@users.noreply.github.com>
2025-01-30 00:19:16 +0000

    Update HeroSecond.tsx

--- src/components/HeroSecond.tsx
+++ src/components/HeroSecond.tsx
@@ -5,15 +5,15 @@ export default function HeroSecond() {
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
         <div style={{ margin: '0 1% 5px 1%' }}>
-          <Image src={'search.svg'} width={'526'} height={'296'} />
+          <Image src={'search.svg'} width={'526'} height={'296'}  alt="Apprenticeship illustration"/>
         </div>
         <div style={{ margin: '0 1% 5px 1%' }}>
-          <Image src={'rangedisplay.svg'} width={'526'} height={'296'} />
+          <Image src={'rangedisplay.svg'} width={'526'} height={'296'} alt='SalaryAverage'/>
         </div>
       </div>
       
       <div style={{ margin: '3%' }}>
-        <Image src={'graph.svg'} width={'1076'} height={'308'} />
+        <Image src={'graph.svg'} width={'1076'} height={'308'} alt='graph illustration'/>
       </div>
     </div>
   );
