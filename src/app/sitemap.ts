bf38e21b02427436d12250a2214968e083bac634
John Name <oluwatobilobasalawu1234@gmail.com>
2025-01-30 20:14:56 +0000

    generated sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,13 @@
+// app/sitemap.ts
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://wapprentice.app',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ];
+}
